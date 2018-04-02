f0ce4a7f432d865250485313a9106ccb08ff28ea
Stefanny <fany-9315@hotmail.com>
2018-04-02 16:45:06 -0600

    -en conexion le cambie la contraseña a root para que a mi me sirviera -index le metí el carrusel -3 archivos agregados para el carrusel

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ require_once("conexion.php");
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel= "stylesheet" type="text/css" href="css/carrusel.css">
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -88,26 +90,71 @@ require_once("conexion.php");
       <div class="container">
         <div class="row">
           <h2 class="text-center">Ultimos trabajos registrados</h2>
-          <?php 
-          //CONSULTAR LOS ULTIMOS 5 PUESTOS PUBLICADOS
-          $sql = "SELECT * FROM puestos WHERE `privado`=0 && `estadoPuesto`=1 ORDER BY `idPuesto` DESC Limit 5 ";//puesto_trabajo es la tabla
-          $result = $conn->query($sql); //result es como un vector que guarda la cantidad de filas consultadas, si es una consulta
-          if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) //fetch pasa el valor siguiente y lo guarda en row funciona como un ++
-            //Devuelve un array (las lineas del query)  que corresponde a la fila recuperada y mueve el puntero de datos interno hacia adelante(++)
-            {
-            ?>
-            <div class="col-md-6 fixHeight">         
-              <h3><?php echo $row['nombrePuesto'];?> </h3>
-              <p><?php echo $row['descripcion'];?></p>
-              <button class="btn btn-default">Vista</button>
-            </div>
-          <?php
-        }
-      }
-      ?>
-        </div>
-      </div>
+
+
+          <!------ Include the above in your HEAD tag ---------->
+
+            <div id="myCarousels" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousels" data-slide-to="0" class="active"></li>
+                    <li data-target="#myCarousels" data-slide-to="1"></li>
+                    <li data-target="#myCarousels" data-slide-to="2"></li>
+                    <li data-target="#myCarousels" data-slide-to="3"></li>
+                    <li data-target="#myCarousels" data-slide-to="4"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+        
+                  <?php 
+                   //CONSULTAR LOS ULTIMOS 5 PUESTOS PUBLICADOS
+                    $sql = "SELECT * FROM puestos WHERE `privado`=0 && `estadoPuesto`=1 ORDER BY `idPuesto` DESC Limit 5 ";//puesto_trabajo es la tabla
+                    $result = $conn->query($sql); //result es como un vector que guarda la cantidad de filas consultadas, si es una consulta
+                    if($result->num_rows > 0) {
+
+                      $primero=1;
+                      while($row = $result->fetch_assoc()) //fetch pasa el valor siguiente y lo guarda en row funciona como un ++
+                      //Devuelve un array (las lineas del query)  que corresponde a la fila recuperada y mueve el puntero de datos interno hacia adelante(++)
+                      {
+
+                        if($primero==1)
+                          echo "<div class='item active'>";
+                        else
+                          echo "<div class='item'>";
+                    ?>
+                
+                          <img src="images/1.jpg" alt="Second slide">
+                          <!-- Static Header -->
+                                  <div class="header-text text-center hidden-xs">
+                                     <div class="main_title ">
+                                    <h2><?php echo $row['nombrePuesto'];?></h2>
+                                    <p><?php echo $row['descripcion'];?></p>
+                                   </div>  
+                                        <div class="">
+                                              <a class="btn btn-lg btn-primary site-btn" href="#">Ver Empleo</a>
+                                          </div>
+                                  </div><!-- /header-text -->
+                        </div>
+                <?php
+                    $primero=0;
+                    }//fin de while
+                  }//fin de if
+                ?>
+
+
+                </div><!--cierre de carousel-inner-->
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#myCarousels" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+            </a>
+            <a class="right carousel-control" href="#myCarousels" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+            </a>
+          </div><!-- /carousel -->
+
+        </div><!--cierre de row-->
+      </div><!--cierre de container-->
     </section>
 
     <!-- Lista de empresas -->
@@ -137,12 +184,17 @@ require_once("conexion.php");
           </div>
         </div>
       </div>
+
     </section>
 
+
+    
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script src="js/carrusel.js"></script>
 
    <script type="text/javascript">
       $(function() {
@@ -155,5 +207,8 @@ require_once("conexion.php");
         $(".fixHeight").height(maxHeight);
       });
     </script>
+
+
+
   </body>
 </html>
---
+++ js/carrusel.js
@@ -0,0 +1,12 @@
+.carousel-control.left,
+.carousel-control.right {
+	opacity: 1;
+	filter: alpha(opacity=100);
+	background-image: none;
+	background-repeat: no-repeat;
+	text-shadow: none;
+	margin-right: 95px;
+}
+.carousel-indicators .active {background-color: #ff5900;}
+.carousel-control {color: #e2e2e2;}
+.carousel-control {width: 90;}
